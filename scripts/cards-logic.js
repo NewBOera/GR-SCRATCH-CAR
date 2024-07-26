9c30ce27cc41e9443531c65223d2757a41a73b0c
Erik Bonifaz <erikbo9@hotmail.com>
2024-07-26 08:38:11 -0600

    chore: cards logic change

--- scripts/cards-logic.js
+++ scripts/cards-logic.js
@@ -2,11 +2,3 @@ const $form = document.getElementById("send-form"),
    firstOffer = document.getElementById("1x"),
    secondOffer = document.getElementById("2x"),
    thirdOffer = document.getElementById("3x");
-
-console.log($form, firstOffer, secondOffer, thirdOffer);
-
-document.addEventListener("click", (event) => {
-   if (event.target === firstOffer) {
-      console.log("oferta1");
-   }
-});
